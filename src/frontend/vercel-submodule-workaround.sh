18235139a1165ae877cafc79a16b1b00d6c581b4
aayush01x <aayusha23@iitk.ac.in>
2024-10-08 14:14:51 +0530

    wht

--- src/frontend/vercel-submodule-workaround.sh
+++ src/frontend/vercel-submodule-workaround.sh
@@ -20,28 +20,3 @@ fi
 
 # stop execution on error - don't let it build if something goes wrong
 set -e
-
-# get submodule commit
-output=`git submodule status --recursive` # get submodule info
-# no_prefix=${output#*-} # get rid of the prefix
-# COMMIT=${no_prefix% *} # get rid of the suffix
-
-# set up an empty temporary work directory
-rm -rf tmp || true # remove the tmp folder if exists
-mkdir tmp # create the tmp folder
-cd tmp # go into the tmp folder
-
-# checkout the current submodule commit
-git init # initialise empty repo
-git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
-git fetch origin master # fetch only the required version
-git checkout master # checkout on the right commit
-
-# move the submodule from tmp to the submodule path
-cd .. # go folder up
-rm -rf tmp/.git # remove .git 
-ls
-mv tmp/* $SUBMODULE_PATH/ # move the submodule to the submodule path
-
-# clean up
-rm -rf tmp # remove the tmp folder
