b4ac27f8058b5a16a7ecd3e7ccb34c08e7f3a524
aayush01x <aayusha23@iitk.ac.in>
2024-10-08 14:14:06 +0530

    take back

--- src/frontend/vercel-submodule-workaround.sh
+++ src/frontend/vercel-submodule-workaround.sh
@@ -1,4 +1,16 @@
+
+# Check if cloning is enabled
+if [ "$TRY_CLONING_TRADINGVIEW" != "1" ]; then
+  echo "Skip clone chart module"
+  exit 0  # Exit without an error, as it's intentional to skip the clone
+fi
+
 # github submodule repo address without https:// prefix
+SUBMODULE_GITHUB=github.com/tradingview/charting_library.git
+
+# .gitmodules submodule path
+SUBMODULE_PATH=public/static
+
 # github access token is necessary
 # add it to Environment Variables on Vercel
 if [ "$GITHUB_ACCESS_TOKEN" == "" ]; then
@@ -16,3 +28,20 @@ output=`git submodule status --recursive` # get submodule info
 
 # set up an empty temporary work directory
 rm -rf tmp || true # remove the tmp folder if exists
+mkdir tmp # create the tmp folder
+cd tmp # go into the tmp folder
+
+# checkout the current submodule commit
+git init # initialise empty repo
+git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
+git fetch origin master # fetch only the required version
+git checkout master # checkout on the right commit
+
+# move the submodule from tmp to the submodule path
+cd .. # go folder up
+rm -rf tmp/.git # remove .git 
+ls
+mv tmp/* $SUBMODULE_PATH/ # move the submodule to the submodule path
+
+# clean up
+rm -rf tmp # remove the tmp folder
